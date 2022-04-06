f0ca2cc7a238d279f5a1ed67ea280ba6365f3b6c
Michał Roszkowski <michalr4550@gmail.com>
2022-04-06 16:54:26 +0200

    updated tests

--- account/tests.py
+++ account/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
+from .models import Profile, Follow
 
 
 class RegistrationTestCase(TestCase):
@@ -25,3 +26,56 @@ class AccountTestCase(TestCase):
     def test_login(self):
         response = self.client.post('/account/login/', self.credentials)
         self.assertEqual(response.status_code, 200)
+
+    def test_logout(self):
+        response = self.client.post('/account/logout/')
+        self.assertEqual(response.status_code, 200)
+
+
+class ViewsTestCase(TestCase):
+
+    def setUp(self):
+        username = 'test'
+        password = 'testpassword123'
+        self.user = User.objects.create_user(username=username)
+        self.user.set_password(password)
+        self.user.save()
+        Profile.objects.create(user=self.user)
+        login = self.client.login(username=username, password=password)
+        self.assertTrue(login)
+
+    def test_follow(self):
+        user_to_follow = User.objects.create_user(username='test2')
+        user_to_follow.set_password('testpassword123')
+        user_to_follow.save()
+        follow = Follow.objects.get_or_create(user_from=self.user, user_to=user_to_follow)
+        self.assertTrue(follow)
+
+    def test_get_user_detail(self):
+        response = self.client.get('/account/users/test/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_user_profile(self):
+        response = self.client.get('/account/edit/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_user_posts(self):
+        response = self.client.get('/user/posts/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_user_comments(self):
+        response = self.client.get('/user/comments/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_user_likes(self):
+        response = self.client.get('/user/likes/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_user_follows(self):
+        response = self.client.get('/user/follows/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_user_bookshelf(self):
+        response = self.client.get('/books/user/bookshelf/')
+        self.assertEqual(response.status_code, 200)
+
--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,50 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Post, Comment
 
-# Create your tests here.
+
+class UserActionsTestCase(TestCase):
+
+    def setUp(self):
+        username = 'test'
+        password = 'testpassword123'
+        self.user = User.objects.create_user(username=username)
+        self.user.set_password(password)
+        self.user.save()
+        self.client.login(username=username, password=password)
+
+    def test_create_post(self):
+        content = 'something'
+        post = self.client.post('/dashboard/', data={'user': self.user,
+                                                          'text': content})
+        self.assertEqual(post.status_code, 302)
+
+    def test_post_like(self):
+        post = Post.objects.create(user=self.user, text='Lorem ipsum')
+        like = self.client.post('/like/', data={'id': post.id,
+                                                'action': 'like'})
+        self.assertEqual(like.status_code, 200)
+
+    def test_post_detail(self):
+        post = Post.objects.create(user=self.user, text='Lorem ipsum')
+        response = self.client.get(f'/post/{post.slug}/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_delete(self):
+        post = Post.objects.create(user=self.user, text='Lorem ipsum')
+        delete = self.client.post(f'/post/delete/{post.id}/')
+        self.assertEqual(delete.status_code, 302)
+
+    def test_create_comment(self):
+        post = Post.objects.create(user=self.user, text='Lorem ipsum')
+        content = 'comment'
+        comment = self.client.post(f'/post/{post.slug}/', data={'author': self.user,
+                                                                'post': post,
+                                                                'text': content})
+        self.assertEqual(comment.status_code, 302)
+
+    def test_comment_delete(self):
+        post = Post.objects.create(user=self.user, text='Lorem ipsum')
+        comment = Comment.objects.create(author=self.user, text='Lorem ipsum', post=post)
+        delete = self.client.post(f'/comment/delete/{comment.id}/')
+        self.assertEqual(delete.status_code, 302)
