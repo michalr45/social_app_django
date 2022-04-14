8675fe1972f59b19543309616c222c1fa8c937eb
Michał Roszkowski <michalr4550@gmail.com>
2022-04-14 13:22:38 +0200

    updated forms labels

--- account/forms.py
+++ account/forms.py
@@ -3,14 +3,9 @@ from django.contrib.auth.models import User
 from .models import Profile
 
 
-class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
-
-
 class UserRegistrationForm(forms.ModelForm):
-    password = forms.CharField(label='Hasło', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Powtórz hasło', widget=forms.PasswordInput)
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
 
     class Meta:
         model = User
@@ -19,7 +14,7 @@ class UserRegistrationForm(forms.ModelForm):
     def clean_password2(self):
         cd = self.cleaned_data
         if cd['password'] != cd['password2']:
-            raise forms.ValidationError('Hasła różnią się')
+            raise forms.ValidationError('Passwords are different')
         return cd['password2']
 
 
